16243e96a28d6dd23b8351280bd1f725ae7ef0f2
nebula-c <suchoi9709@gmail.com>
2025-05-02 11:05:45 +0900

    Default init

--- setup.py
+++ setup.py
@@ -4,5 +4,13 @@ setup(
     name='GELATI',
     version='0.1',
     packages=find_packages(where='src'),
+	install_requires=[
+        'PyQt6',
+    ],
+    entry_points={
+        'console_scripts': [
+            'myapp = gelati.main:main',
+        ],
+    }
 )
 
