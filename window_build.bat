08a1defbb9aa50beb45526a560622481e69461a1
nebula-c <suchoi9709@gmail.com>
2025-05-20 14:44:53 +0900

    Fixed error

--- window_build.bat
+++ window_build.bat
@@ -3,5 +3,5 @@ pyinstaller --noconfirm --windowed --name gelati --hidden-import=PyQt6.QtCharts 
 
 rmdir /s /q build
 del /f gelati.spec
-move dist\gelati\gelati.app .\
+move dist\gelati\gelati .\
 rmdir /s /q dist
