ca5fc075a3d91b72d9c981c1da13d5443bbe711f
nebula-c <suchoi9709@gmail.com>
2025-05-20 14:31:48 +0900

    Fixed error

--- window_build.bat
+++ window_build.bat
@@ -1,9 +1,5 @@
-pyinstaller --noconfirm --windowed --name gelati src\gelati\main.py
-pyinstaller --noconfirm --windowed --name gelati \
-  --hidden-import=PyQt6.QtCharts \
-  --add-data src/gelati/images/gelati_logo2.png:images \
-  --add-data src/gelati/images/gelati_logo1.png:images \
-  src/gelati/main.py
+
+pyinstaller --noconfirm --windowed --name gelati --hidden-import=PyQt6.QtCharts --add-data src/gelati/images/gelati_logo2.png:images --add-data src/gelati/images/gelati_logo1.png:images src/gelati/main.py
 
 rmdir /s /q build
 del /f gelati.spec
